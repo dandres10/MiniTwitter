19ca0061ba2adefa24d8b4a2c1361a0307ee41e4
Andrés León León <dandresleon64@gmail.com>
2020-01-22 22:34:40 -0500

    pantalla para crear un usuario

--- app/src/main/java/com/example/minitwitter/MainActivity.java
+++ app/src/main/java/com/example/minitwitter/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.minitwitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnLogin;
+    TextView tvGoSingUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        tvGoSingUp = findViewById(R.id.textViewGoSingUp);
+
+        btnLogin.setOnClickListener(this);
+        tvGoSingUp.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id){
+            case R.id.buttonLogin:
+            break;
+            case R.id.textViewGoSingUp:
+                goTOSingUp();
+                break;
+        }
+    }
+
+    private void goTOSingUp() {
+        Intent i = new Intent(MainActivity.this, SingUpActivity.class);
+        startActivity(i);
     }
 }
