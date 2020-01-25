245abc3708c7ad4a4b82c3ede49bb6dbf2d4cb43
Andrés León León <dandresleon64@gmail.com>
2020-01-25 11:42:40 -0500

    clase 17

--- app/src/main/java/com/example/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/example/minitwitter/ui/DashboardActivity.java
@@ -2,42 +2,34 @@ package com.example.minitwitter.ui;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.minitwitter.common.Constantes;
-import com.example.minitwitter.common.SharedPreferencesManager;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 import com.example.minitwitter.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
 
-            switch (menuItem.getItemId()) {
+            switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_tweets_like:
+
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+
                     return true;
             }
 
@@ -51,19 +43,12 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        getSupportActionBar().hide();
+
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
 
-        String token = SharedPreferencesManager.getSomeStringValue(Constantes.PREF_TOKEN);
-        Toast.makeText(this, "Token: "+token, Toast.LENGTH_SHORT).show();
 
     }
 
