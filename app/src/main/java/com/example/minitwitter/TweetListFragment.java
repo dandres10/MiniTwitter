af9986564597f11400d4a9a2c08eaedee3bf9532
Andrés León León <dandresleon64@gmail.com>
2020-01-25 12:54:26 -0500

    clase 18

--- app/src/main/java/com/example/minitwitter/TweetListFragment.java
+++ app/src/main/java/com/example/minitwitter/TweetListFragment.java
@@ -12,24 +12,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.minitwitter.dummy.DummyContent;
-import com.example.minitwitter.dummy.DummyContent.DummyItem;
+import com.example.minitwitter.retrofit.response.Tweet;
 
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
+
 public class TweetListFragment extends Fragment {
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
     private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
+    RecyclerView recyclerView;
+    MyTweetRecyclerViewAdapter adapater;
+    List<Tweet> tweetList;
+
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -65,47 +62,22 @@ public class TweetListFragment extends Fragment {
         // Set the adapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView = (RecyclerView) view;
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new MyTweetRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+            loadTweetData();
         }
         return view;
     }
 
+    private void loadTweetData() {
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
-        }
+        adapater = new MyTweetRecyclerViewAdapter(getActivity(), tweetList);
+        recyclerView.setAdapter(adapater);
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(DummyItem item);
-    }
 }
